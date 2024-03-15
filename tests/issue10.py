3636bda5f6ce5292bf8f5e3ceca77bf1b9b1b940
Marc Mengel <mengel@fnal.gov>
2024-03-14 22:59:37 -0500

    more fun

--- tests/issue10.py
+++ tests/issue10.py
@@ -14,33 +14,32 @@ def test_issue_10(auth):
         set -x
         metacat dataset create {user}:steve_test_retire_{start_ts}
         metacat file declare {user}:1mbtestfile.st2024{start_ts} {user}:steve_test_retire_{start_ts} -s 1024000 -c adler32:a0e10001
-        metacat file declare {user}:1mbtestfile.st2024{start_ts}.child {user}:steve_test_retire_{start_ts} -s 1024000 -c adler32:a0e10001 --parents {user}:1mbtestfile.st2024{start_ts}
+        metacat file declare {user}:1mbtestfile.st2024{start_ts}.child1 {user}:steve_test_retire_{start_ts} -s 1024000 -c adler32:a0e10001 --parents {user}:1mbtestfile.st2024{start_ts}
         metacat file declare {user}:1mbtestfile.st2024{start_ts}.child2 {user}:steve_test_retire_{start_ts} -s 1024000 -c adler32:a0e10001 --parents {user}:1mbtestfile.st2024{start_ts}
         metacat dataset files {user}:steve_test_retire_{start_ts}
         metacat file show {user}:1mbtestfile.st2024{start_ts} -l
-        metacat file show {user}:1mbtestfile.st2024{start_ts}.child -l
+        metacat file show {user}:1mbtestfile.st2024{start_ts}.child1 -l
         metacat file show {user}:1mbtestfile.st2024{start_ts}.child2 -l
     """)
 
     with os.popen(f"""
         set -x
         metacat query children '(files from {user}:steve_test_retire_{start_ts})'
-        metacat query parents ' (files from {user}:steve_test_retire_{start_ts})'
     """) as fin:
          out1 = fin.read()
 
     os.system(f"""
         set -x
-        metacat file retire {user}:1mbtestfile.st2024{start_ts}.child
+        metacat file retire {user}:1mbtestfile.st2024{start_ts}.child1
     """)
 
     with os.popen(f"""
         set -x
         metacat query children '(files from {user}:steve_test_retire_{start_ts})'
-        metacat query parents ' (files from {user}:steve_test_retire_{start_ts})'
     """) as fin:
          out2 = fin.read()
     
     # Make sure our file is in the pre-remove query  but  not the post remove...
-    assert(out1.find( f"{user}:1mbtestfile.st2024{start_ts}.child") >= 0)
-    assert(out2.find( f"{user}:1mbtestfile.st2024{start_ts}.child") < 0)
+    print(f"comparing: '{out1}' '{out2}'")
+    assert(out1.find( f"{user}:1mbtestfile.st2024{start_ts}.child1") >= 0)
+    assert(out2.find( f"{user}:1mbtestfile.st2024{start_ts}.child1") < 0)
