8804a86dd61e28d2e10ac7c8cf090a41ed983427
Marc Mengel <mengel@fnal.gov>
2024-07-23 17:32:39 -0500

    checksum_checks tested

--- tests/test_checksum_checks.py
+++ tests/test_checksum_checks.py
@@ -23,25 +23,6 @@ def test_metacat_dataset_create(auth, tst_ds):
 
 def test_metacat_file_declare_bad_chksum_1(auth, tst_file_md_list, tst_ds):
     # add a file we did before, with ck1 on the filename, and
-    # a 4-char adler32 checksum -- this should fail due to the checksum
-    # being short.
-    tst_ds =  tst_ds + "_cksum"
-    md = tst_file_md_list[0].copy()
-    md["name"] = "ck1_" + md["name"]
-    md["checksums"]["adler32"] = md["checksums"]["adler32"][:4]
-    print("metadata:\n", json.dumps(md))
-    with open("mdf1", "w") as mdf:
-        json.dump(md, mdf)
-    with os.popen(f"metacat file declare -f mdf1 {tst_ds} 2>&1", "r") as fin:
-        data = fin.read()
-    print(f"got data: '{data}'")
-    os.unlink("mdf1")
-    assert data.find("validation error") > 0
-    assert data.find("adler32: value is too short") > 0
-    assert False
-
-def test_metacat_file_declare_bad_chksum_1(auth, tst_file_md_list, tst_ds):
-    # add a file we did before, with ck1 on the filename, and
     # a funky adler32 checksum, should report bad digits
     tst_ds =  tst_ds + "_cksum"
     md = tst_file_md_list[0].copy()
